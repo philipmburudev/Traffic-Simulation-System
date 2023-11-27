01ef2c6f7aabbd7648928dc24e8876400941c665
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2023-11-27 14:55:56 +0000

    lane class changes

---
+++ Lane.java
@@ -0,0 +1,21 @@
+
+import java.util.Queue;
+
+
+public class Lane {
+    //queues to hold cars 
+    private Queue<Vehicle> inputlane;
+    private Queue<Vehicle> outputlane;
+    //lane number and name
+    private int laneNumber;
+    private String laneName;
+
+    public Lane(int laneNumber, String laneName) {
+        this.laneNumber = laneNumber;
+        this.laneName = laneName;
+    }
+
+    
+
+    
+}
