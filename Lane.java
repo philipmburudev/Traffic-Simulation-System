6203a4a41a89ec83c9b01ccff06cc6a352db3056
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2023-11-27 15:57:47 +0000

    yh nothing much

--- Lane.java
+++ Lane.java
@@ -13,48 +13,48 @@ import java.util.LinkedList;
         // traffic light obj
         private TrafficLight trafficLight = new TrafficLight();
 
-    public Lane(int laneNumber, String laneName) {
-        this.laneNumber = laneNumber;
-        this.laneName = laneName;
-        this.trafficLight.setCurrentColor("RED");
-    }
+        public Lane(int laneNumber, String laneName) {
+            this.laneNumber = laneNumber;
+            this.laneName = laneName;
+            this.trafficLight.setCurrentColor("RED");
+        }
 
-    // setter for lane number
-    public void setLaneNumber(int laneNumber) {
-        this.laneNumber = laneNumber;
-    }
+        // setter for lane number
+        public void setLaneNumber(int laneNumber) {
+            this.laneNumber = laneNumber;
+        }
 
-    // getter for lane number
-    public int getLaneNumber() {
-        return laneNumber;
-    }
+        // getter for lane number
+        public int getLaneNumber() {
+            return laneNumber;
+        }
 
-    // setter for lane name
-    public void setLaneName(String laneName) {
-        this.laneName = laneName;
-    }
+        // setter for lane name
+        public void setLaneName(String laneName) {
+            this.laneName = laneName;
+        }
 
-    // getter for lane name
-    public String getLaneName() {
-        return laneName;
-    }
+        // getter for lane name
+        public String getLaneName() {
+            return laneName;
+        }
 
-    // put a Vehicle in a lane
-    public void putVehicle(Vehicle vehicle) {
-        inputlane.add(vehicle);
-    }
+        // put a Vehicle in a lane
+        public void putVehicle(Vehicle vehicle) {
+            inputlane.add(vehicle);
+        }
 
-    public void putVehicleInOutputLane(Vehicle vehicle) {
-        outputlane.add(vehicle);
-    }
+        public void putVehicleInOutputLane(Vehicle vehicle) {
+            outputlane.add(vehicle);
+        }
 
-    public Queue<Vehicle> getVehiclesFromOutputLane() {
-        return outputlane;
-    }
+        public Queue<Vehicle> getVehiclesFromOutputLane() {
+            return outputlane;
+        }
 
-    public Queue<Vehicle> getVehiclesFromInputLane() {
-        return inputlane;
-    }
+        public Queue<Vehicle> getVehiclesFromInputLane() {
+            return inputlane;
+        }
 
     
 
