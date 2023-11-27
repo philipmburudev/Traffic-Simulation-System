b408d0be7f616ab196979e0f95dd6f1cb68b97b5
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2023-11-27 15:32:52 +0000

    yeahhh

--- Lane.java
+++ Lane.java
@@ -1,18 +1,40 @@
 
 import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Queue;
 
 
-public class Lane {
-    //queues to hold cars 
-    private Queue<Vehicle> inputlane;
-    private Queue<Vehicle> outputlane;
-    //lane number and name
-    private int laneNumber;
-    private String laneName;
+
+    public class Lane {
+        private Queue<Vehicle> inputlane = new LinkedList<>();
+        private Queue<Vehicle> outputlane = new LinkedList<>();
+        //lane number and name
+        private int laneNumber;
+        private String laneName;
+        // traffic light obj
+        private TrafficLight trafficLight = new TrafficLight();
 
     public Lane(int laneNumber, String laneName) {
         this.laneNumber = laneNumber;
         this.laneName = laneName;
+        this.trafficLight.setCurrentColor("RED");
+    }
+
+    // put a Vehicle in a lane
+    public void putVehicle(Vehicle vehicle) {
+        inputlane.add(vehicle);
+    }
+
+    public void putVehicleInOutputLane(Vehicle vehicle) {
+        outputlane.add(vehicle);
+    }
+
+    public Queue<Vehicle> getVehiclesFromOutputLane() {
+        return outputlane;
+    }
+
+    public Queue<Vehicle> getVehiclesFromInputLane() {
+        return inputlane;
     }
 
     
--- TrafficLight.java
+++ TrafficLight.java
@@ -1,6 +1,3 @@
-import java.util.Scanner;
-
-
 public class TrafficLight{
 
    
@@ -10,9 +7,24 @@ public enum trafficlight{
   GREEN
 }
 
+trafficlight currentColor;
+
 
 //default constructor to initialize the traffic light
 public TrafficLight() {
+
+}
+
+// setter for current color
+public void setCurrentColor(String color) {
+  if (color.compareTo("RED")==0) this.currentColor = trafficlight.RED;
+  else if (color.compareTo("YELLOW")==0) this.currentColor = trafficlight.YELLOW;
+  else if(color.compareTo("GREEN")==0) this.currentColor = trafficlight.GREEN;
+}
+
+// getter for current color
+public trafficlight getCurrentColor() {
+  return currentColor;
 }
 
 
@@ -20,27 +32,40 @@ public TrafficLight() {
 public class Main {
 
   public static void main(String[] args) {
-    trafficlight color = trafficlight.YELLOW;
+    // System.out.println("Type traffic color");
+    Lane lane1 = new Lane(1, "lane1");
+    Lane lane2 = new Lane(2, "lane2");
+    Lane lane3 = new Lane(3, "lane3");
+    Lane lane4 = new Lane(4, "lane4");
 
-    System.out.println("Type traffic color");
-    Scanner scan = new Scanner(System.in);
-    String currentcolor = scan.nextLine();
-    
-    if(currentcolor.compareTo("RED")==0) color = trafficlight.RED;
-    else if (currentcolor.compareTo("YELLOW")==0) color = trafficlight.YELLOW;
-    else if(currentcolor.compareTo("GREEN")==0) color = trafficlight.GREEN;
-        
-    switch(color) {
-      case RED:
-        System.out.println("Lane 1");
-        break;
-      case YELLOW:
-         System.out.println("Lane 2");
-        break;
-      case GREEN:
-        System.out.println("Lane 3");
-        break;
+    Vehicle c1 = new Vehicle("normal", 1, "car1", 2);
+    Vehicle c2 = new Vehicle("normal", 2, "car2", 2);
+    Vehicle c3 = new Vehicle("emergency", 3, "car3", 2);
+    Vehicle c4 = new Vehicle("normal", 4, "car4", 2);
+    Vehicle c5 = new Vehicle("normal", 5, "car5", 2);
+
+    lane1.putVehicleInOutputLane(c1);
+    lane1.putVehicleInOutputLane(c2);
+    lane1.putVehicleInOutputLane(c3);
+    lane1.putVehicleInOutputLane(c4);
+    lane1.putVehicleInOutputLane(c5);
+
+    System.out.println("lane1");
+    for (Vehicle v : lane1.getVehiclesFromOutputLane()) {
+      System.out.println(v.getName());
     }
+    System.out.println("lane2");
+
+    lane2.putVehicle(lane1.getVehiclesFromOutputLane().remove());
+    lane2.putVehicle(lane1.getVehiclesFromOutputLane().remove());
+    lane2.putVehicle(lane1.getVehiclesFromOutputLane().remove());
+    lane2.putVehicle(lane1.getVehiclesFromOutputLane().remove());
+    lane2.putVehicle(lane1.getVehiclesFromOutputLane().remove());
+
+    for (Vehicle v : lane2.getVehiclesFromInputLane()) {
+      System.out.println(v.getName());
+    }
+  }
 
 
     
@@ -57,4 +82,4 @@ public class Main {
    
   }
 }
-}
+
