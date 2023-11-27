ebc79e898a1bd7e20c68f28f3552d26c3d269bf8
Philip Mburu <philip.wanyoike@ashesi.edu.gh.com>
2023-11-27 14:17:13 +0000

    firstcommit

---
+++ TrafficLight.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+public class TrafficLight{
+   
+public enum trafficlight{
+  RED,
+  YELLOW,
+  GREEN
+}
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    trafficlight color = trafficlight.YELLOW;
+
+    System.out.println("Type color");
+    Scanner scan = new Scanner(System.in);
+    String currentcolor = scan.nextLine();
+    
+    
+    if (currentcolor.compareTo("YELLOW")==0) color = trafficlight.YELLOW;
+    else if(currentcolor.compareTo("RED")==0) color = trafficlight.RED;
+    else if(currentcolor.compareTo("GREEN")==0) color = trafficlight.GREEN;
+    switch(color) {
+      case RED:
+        System.out.println("Lane 1");
+        break;
+      case YELLOW:
+         System.out.println("Lane 2");
+        break;
+      case GREEN:
+        System.out.println("Lane 3");
+        break;
+    }
+  }
+}
+}
