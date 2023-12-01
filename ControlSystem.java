6f6023202daee518ac0b382446bee8a6d383e2f9
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2023-12-01 22:28:49 +0000

    yh

--- ControlSystem.java
+++ ControlSystem.java
@@ -42,17 +42,14 @@ public class ControlSystem {
         int[][] directions = {directions1, directions2, directions3, directions4};
         // Generate a random direction (e.g., 1, 2, 3, 4)
         int laneMumber = lane.getLaneNumber();
-
         directions2 = directions[laneMumber - 1];
-        
-
         long arrivalTime = System.currentTimeMillis();
 
         // generating 1-5 vehicles
         int numOfVehicles = new Random().nextInt(1,6);
         for (int i=0; i<numOfVehicles; i++) {
             int direction = directions2[new Random().nextInt(directions2.length)];
-            
+
             // Generate emergency vehicles with a certain probability (e.g., 10%)
             boolean isEmergency = new Random().nextInt(10) == 0;
 
@@ -87,29 +84,38 @@ public class ControlSystem {
          * lane3 is south
          * lane4 is west
          */
+        lane1.trafficLight.setCurrentColor("GREEN");
         while (!lane1.getOutputLane().isEmpty() || 
                !lane2.getOutputLane().isEmpty() ||
                !lane3.getOutputLane().isEmpty() ||
                !lane4.getOutputLane().isEmpty()) {
             // Check the traffic light
+            System.out.println("Traffic light 1: " + lane1.trafficLight.getCurrentColor());
+            System.out.println("Traffic light 2: " + lane2.trafficLight.getCurrentColor());
+            System.out.println("Traffic light 3: " + lane3.trafficLight.getCurrentColor());
+            System.out.println("Traffic light 4: " + lane4.trafficLight.getCurrentColor());
             if (lane1.trafficLight.getCurrentColor() == TrafficLight.trafficlight.GREEN) {
                 // Move vehicles from lane1 to lane2
                 ControlSystem.moveVehiclesOut(lanes, lane1);
+                lane1.showAllVehicles();
                 // Generate vehicles for lane1
                 // ControlSystem.generateVehicle(lane1);
             } else if (lane2.trafficLight.getCurrentColor() == TrafficLight.trafficlight.GREEN) {
                 // Move vehicles from lane2 to lane3
                 ControlSystem.moveVehiclesOut(lanes, lane2);
+                lane2.showAllVehicles();
                 // Generate vehicles for lane2
                 // ControlSystem.generateVehicle(lane2);
             } else if (lane3.trafficLight.getCurrentColor() == TrafficLight.trafficlight.GREEN) {
                 // Move vehicles from lane3 to lane4
                 ControlSystem.moveVehiclesOut(lanes, lane3);
+                lane3.showAllVehicles();
                 // Generate vehicles for lane3
                 // ControlSystem.generateVehicle(lane3);
             } else if (lane4.trafficLight.getCurrentColor() == TrafficLight.trafficlight.GREEN) {
                 // Move vehicles from lane4 to lane1
                 ControlSystem.moveVehiclesOut(lanes, lane4);
+                lane4.showAllVehicles();
                 // Generate vehicles for lane4
                 // ControlSystem.generateVehicle(lane4);
             }
